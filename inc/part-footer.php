ab4a0b62713d1433cdca2f546878057a5ef51110
Velocity Developer <76415135+VelocityDeveloper@users.noreply.github.com>
2023-10-16 13:40:13 +0700

    Add files via upload

---
+++ inc/part-footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer container text-center px-2 px-md-0" id="colophon">
+    <div class="site-info shadow border-light border border-top-0 px-3 py-2 py-md-4">
+        <small class="text-secondary">
+            Copyright © <?php echo date("Y"); ?> <?php echo get_bloginfo('name'); ?>. All Rights Reserved.
+        </small>
+        <br>
+        <small class="opacity-25" style="font-size: .7rem;">
+            Design by <a class="text-muted" href="https://velocitydeveloper.com" target="_blank" rel="noopener noreferrer"> Velocity Developer </a>
+        </small>
+    </div>
+    <!-- .site-info -->
+</footer>
