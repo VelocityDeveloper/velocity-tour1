556b61f19873bf1c198ff34fad2c075b37ef1939
irawanyulianto <71108779+irawanyulianto@users.noreply.github.com>
2025-06-07 15:27:06 +0700

    Update part-header.php

--- inc/part-header.php
+++ inc/part-header.php
@@ -1,18 +1,53 @@
-<nav id="main-navi" class="navbar navbar-expand-md d-block navbar-light pb-0 px-0 pt-0" aria-labelledby="main-nav-label">
+<?php
+$telepon = velocitytheme_option('kontak_telepon');
+$whatsapp = velocitytheme_option('kontak_whatsapp');
+$email = velocitytheme_option('kontak_email');
+?>
 
-    <h2 id="main-nav-label" class="screen-reader-text">
-        <?php esc_html_e('Main Navigation', 'justg'); ?>
-    </h2>
+<div class="bg-primary p-1 text-white text-center">
+    <?php if (!empty($telepon)) { ?>
+        <small class="my-1 mx-2 d-inline-block align-middle">
+            <a href="tel:<?php echo $telepon; ?>" class="text-white" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-top me-1 bi bi-telephone" viewBox="0 0 16 16"> <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/></svg> <?php echo $telepon; ?>
+            </a>
+        </small>
+    <?php } if (!empty($whatsapp)) {
+        $whatsapp = $whatsapp ? preg_replace('/[^0-9]/', '', $whatsapp) : $whatsapp;
+        if (substr($whatsapp, 0, 1) == 0) {
+            $whatsapp_number    = substr_replace($whatsapp, '62', 0, 1);
+        } ?>
+        <small class="my-1 mx-2 d-inline-block align-middle">
+            <a href="https://wa.me/<?php echo $whatsapp_number; ?>" class="text-white" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="align-top me-1 bi bi-whatsapp" viewBox="0 0 16 16"> <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/> </svg> <?php echo $whatsapp; ?>
+            </a>
+        </small>
+    <?php } if (!empty($email)) { ?>
+        <small class="my-1 mx-2 d-inline-block align-middle">
+            <a href="mailto:<?php echo $email; ?>" class="text-white" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="align-top me-1 bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/> </svg> <?php echo $email; ?>
+            </a>
+        </small>
+    <?php } ?>
+</div>
 
-    <div class="d-block">
-        <div class="w-100">
-            <?php if (has_header_image()) {
-                echo '<a href="'.get_home_url().'">';
-                    echo '<img class="w-100" src="'.esc_url(get_header_image()).'" />';
-                echo '</a>';
-            } ?>
+
+<div class="container row mx-auto py-2 align-items-center">
+    <div class="col-8 col-sm-4 col-md-3 pe-0">
+        <?php echo the_custom_logo(); ?>
+    </div>
+    <nav id="main-navi" class="col-4 col-sm-8 col-md-9 navbar navbar-expand-md d-block navbar-light p-0" aria-labelledby="main-nav-label">
+
+        <div id="main-nav-label" class="screen-reader-text">
+            <?php esc_html_e('Main Navigation', 'justg'); ?>
         </div>
-        <div class="menu-frame">
+
+        <div class="menu-header position-relative py-md-2 px-md-3">
+
+            <button class="btn btn-sm btn-link ms-auto d-md-none d-flex align-items-center" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNavOffcanvas" aria-controls="navbarNavOffcanvas" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'justg'); ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>
+            </button>
 
             <div class="offcanvas offcanvas-start" tabindex="-1" id="navbarNavOffcanvas">
 
@@ -27,32 +62,18 @@
                         'theme_location'  => 'primary',
                         'container_class' => 'offcanvas-body',
                         'container_id'    => '',
-                        'menu_class'      => 'navbar-nav navbar-light justify-content-start flex-md-wrap flex-grow-1',
+                        'menu_class'      => 'navbar-nav navbar-light justify-content-end flex-md-wrap flex-grow-1',
                         'fallback_cb'     => '',
                         'menu_id'         => 'primary-menu',
                         'depth'           => 4,
                         'walker'          => new justg_WP_Bootstrap_Navwalker(),
                     )
-                ); ?>
+                );
+                ?>
+            </div><!-- .offcanvas -->
 
-                </div><!-- .offcanvas -->
-            </div>
-    </div>
+        </div>
 
-    <?php $bannerheader = velocitytheme_option('image_bannerheader', ''); ?>
-    <div class="part_bannerheader">
-        <?php if ($bannerheader) : ?>
-            <img class="img-fluid w-100" src="<?php echo $bannerheader; ?>" alt="Banner Header" loading="lazy">
-        <?php endif; ?>
-    </div>
+    </nav><!-- .site-navigation -->
 
-    <div class="menu-header d-md-none position-relative bg-theme" data-bs-theme="dark">
-
-        <button class="navbar-toggler text-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNavOffcanvas" aria-controls="navbarNavOffcanvas" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'justg'); ?>">
-            <span class="navbar-toggler-icon"></span>
-            <small>Menu</small>
-        </button>
-
-    </div>
-
-</nav><!-- .site-navigation -->
+</div>
