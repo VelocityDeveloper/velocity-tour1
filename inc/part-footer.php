0f970f6bf29bee1b24c0e5bb024351d3a3c75d8b
irawanyulianto <71108779+irawanyulianto@users.noreply.github.com>
2025-06-07 15:27:59 +0700

    Update part-footer.php

--- inc/part-footer.php
+++ inc/part-footer.php
@@ -1,11 +1,20 @@
-<footer class="site-footer container text-center px-2 px-md-0" id="colophon">
-    <div class="site-info shadow border-light border border-top-0 px-3 py-2 py-md-4">
-        <small class="text-secondary">
+<footer class="site-footer text-center pt-3 px-md-0 bg-dark text-white" id="colophon">
+    <div class="row footer-widget container text-start mx-auto py-5">
+        <?php for ($x = 1; $x <= 3; $x++) { ?>
+            <?php if (is_active_sidebar('footer-widget-' . $x)) { ?>
+                <div class="col-md">
+                    <?php dynamic_sidebar('footer-widget-' . $x); ?>
+                </div>
+            <?php } ?>
+        <?php } ?>
+    </div>
+    <div class="site-info px-3 py-2 py-md-4 bg-theme">
+        <small class="text-white">
             Copyright © <?php echo date("Y"); ?> <?php echo get_bloginfo('name'); ?>. All Rights Reserved.
         </small>
         <br>
         <small class="opacity-25" style="font-size: .7rem;">
-            Design by <a class="text-muted" href="https://velocitydeveloper.com" target="_blank" rel="noopener noreferrer"> Velocity Developer </a>
+            Design by <a class="text-white" href="https://velocitydeveloper.com" target="_blank" rel="noopener noreferrer"> Velocity Developer </a>
         </small>
     </div>
     <!-- .site-info -->
