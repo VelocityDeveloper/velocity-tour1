dd028983c6cda7b89caebf00acef0fdc7282fba7
irawanyulianto <71108779+irawanyulianto@users.noreply.github.com>
2025-06-07 15:27:23 +0700

    Update enqueue.php

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -19,6 +19,34 @@ if (!function_exists('justg_child_enqueue_parent_style')) {
         $parenthandle = 'parent-style';
         $theme = wp_get_theme();
 
+        // Jika plugin tidak me-load CSS
+        if (
+            !wp_style_is('magnific-popup-styles', 'enqueued') &&
+            !wp_style_is('magnific-popup-styles', 'registered')
+        ) {
+            wp_enqueue_style(
+                'velocity-tour1-magnific-popup-styles',
+                'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/magnific-popup.min.css',
+                [],
+                '1.0.0'
+            );
+        }
+
+        // Jika plugin tidak me-load JS
+        if (
+            !wp_script_is('magnific-popup-script', 'enqueued') &&
+            !wp_script_is('magnific-popup-script', 'registered')
+        ) {
+            wp_enqueue_script(
+                'velocity-tour1-magnific-popup-script',
+                'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/jquery.magnific-popup.min.js',
+                ['jquery'],
+                '1.0.0',
+                true
+            );
+        }
+
+
         // Load the stylesheet
         wp_enqueue_style(
             $parenthandle,
@@ -27,10 +55,18 @@ if (!function_exists('justg_child_enqueue_parent_style')) {
             $theme->parent()->get('Version')
         );
 
+        // load bootstrap-icons
+        wp_enqueue_style(
+            'velocity-tour1-bootstrap-icons',
+            get_stylesheet_directory_uri() . '/css/bootstrap-icons.css',
+            array(),
+            filemtime(get_stylesheet_directory() . '/css/bootstrap-icons.css') // ini untuk cache busting
+        );
+
         // $css_version = $theme->parent()->get('Version') . '.' . filemtime( get_stylesheet_directory() . '/css/custom.css' );
         $css_version = $theme->parent()->get('Version');
         wp_enqueue_style(
-            'custom-style',
+            'velocity-tour1-custom-style',
             get_stylesheet_directory_uri() . '/css/custom.css',
             array(),  // if the parent theme code has a dependency, copy it to here
             $css_version
@@ -38,7 +74,8 @@ if (!function_exists('justg_child_enqueue_parent_style')) {
 
         wp_enqueue_style(
             'velocity-google-fonts',
-            'https://fonts.googleapis.com/css?family=Playfair+Display|Space+Mono|Stint+Ultra+Expanded&display=swap', false
+            'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+            false
         );
 
         wp_enqueue_style(
