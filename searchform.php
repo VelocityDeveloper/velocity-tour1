f1f010b5ff0175fc5474e75d95663988734d7402
irawanyulianto <71108779+irawanyulianto@users.noreply.github.com>
2025-06-07 15:24:34 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
 		<input class="field form-control rounded-0" id="s" name="s" type="text"
 			placeholder="<?php esc_attr_e( 'Search', 'justg' ); ?>" value="<?php the_search_query(); ?>" required>
 		<span class="input-group-append">
-            <button type="submit" class="submit btn bg-theme btn-sm rounded-0">
+            <button type="submit" class="submit btn bg-theme rounded-0">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                 </svg>
